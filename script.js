d8b6abf9e23f809085d2a10363ab09f5fb6e6951
Retky <jose.arredondo.a@outlook.com>
2022-03-14 13:53:31 -0600

    Add 'addBook' function, to add books in bookList array

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+// Holds the books objects
 const bookList = [];
+
+
+// Add book to bookList(w- Title & Author)
+function addBook(title, author) {
+  const book = {
+      title: title,
+      author: author
+  };
+  bookList.push(book);
+}
