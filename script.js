43972c5cc56c94e55fe7951a658fb4c90222fec5
Godwin Nwachukwu <nwgwin@gmail.com>
2022-03-14 21:25:25 +0100

    add function to display each book inside the book list

--- script.js
+++ script.js
@@ -15,3 +15,29 @@ function addBook(title, author) {
 function removeBook(title, author) {
   bookList = bookList.filter(book => book.title !== title)
 }
+
+// Create book card for each e in bookList
+const bookSection = document.getElementById('bookList');
+
+function displayBook() {
+  bookList.forEach((book) => {
+    const bDiv = document.createElement('div');
+    const bTitle = document.createElement('p');
+    const bAuthor = document.createElement('p');
+    const bBtn = document.createElement('button');
+    const bLine = document.createElement('hr');
+
+    bTitle.innerHTML = book.title;
+    bAuthor.innerHTML = book.author;
+    bBtn.innerHTML = 'Remove';
+
+    bDiv.appendChild(bTitle);
+    bDiv.appendChild(bAuthor);
+    bDiv.appendChild(bBtn);
+    bDiv.appendChild(bLine);
+
+    bookSection.appendChild(bDiv);
+  });
+}
+
+displayBook()
