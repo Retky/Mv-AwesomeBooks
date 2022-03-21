528713caa313d4363a99653ef0f24d6260790305
Retky <jose.arredondo.a@outlook.com>
2022-03-21 12:25:02 -0600

    Modify 'fromLocalStorage' to module

--- js/index.js
+++ js/index.js
@@ -1,1 +1,6 @@
 import {bookList} from './modules/books.mjs'
+import {fromLS} from './modules/localStorage.mjs'
+
+fromLS()
+
+console.log(bookList);
--- js/modules/localStorage.mjs
+++ js/modules/localStorage.mjs
@@ -2,7 +2,15 @@ import {bookList} from './books.mjs'
 
 const toLS = () => {
   const to = JSON.stringify(bookList.list);
-  localStorage.setItem('books', toLS);
+  localStorage.setItem('books', to);
 }
 
-export {toLS}
+const fromLS = () => {
+  if (localStorage.books) {
+    const from = JSON.parse(localStorage.books);
+    bookList.list = from;
+  }
+
+}
+
+export {toLS, fromLS}
